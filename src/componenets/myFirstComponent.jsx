0e5eb6957ede79127aa015e28b44cb1574bffb83
Haseeb Asif <36411814+HaseebAsif@users.noreply.github.com>
2022-06-10 11:32:47 +0500

    Feat: First React JS Component

---
+++ src/componenets/myFirstComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MyFirstComponent = () => {
+  return (
+    <div>
+        <h1>This is my First Ever React JS Component</h1>
+        <h2>Coding starts here</h2>
+    </div>
+  )
+}
+
+export default MyFirstComponent
---
+++ src/componenets/mySecondComponent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MySecondComponent = () => {
+  return <div>My Second Component</div>;
+};
+
+export default MySecondComponent;
