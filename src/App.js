94964fc0b3aa16c3f4cbc93d4ec7d920a94483b3
Haseeb Asif <36411814+HaseebAsif@users.noreply.github.com>
2022-06-10 16:32:30 +0500

    Feat: CSS Styling added to the react app

--- src/App.js
+++ src/App.js
@@ -3,13 +3,29 @@ import MyFirstComponent from "./componenets/myFirstComponent";
 import MySecondComponent from "./componenets/mySecondComponent";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello Word</h1>
+  const centerDiv = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    flexDirection: "column",
+    height: "100vh",
+  };
 
-      <p>This is my first React JS Application</p>
-      <MyFirstComponent />
-      <MySecondComponent />
+  const header = {
+    backgroundColor: "yellow",
+  };
+
+  return (
+    <div className="App" style={centerDiv}>
+      <h1 className="app-heading">Hello World</h1>
+
+      <p style={{ color: "red" }} className="paragraph">
+        This is my first React JS Application
+      </p>
+      <div style={header}>
+        <MyFirstComponent />
+        <MySecondComponent />
+      </div>
     </div>
   );
 }
--- src/componenets/myFirstComponent.jsx
+++ src/componenets/myFirstComponent.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import './myFirstComponent.css'
 
 const MyFirstComponent = () => {
   return (
     <div>
-        <h1>This is my First Ever React JS Component</h1>
+        <h1 className='component-heading'>This is my First Ever React JS Component</h1>
         <h2>Coding starts here</h2>
     </div>
   )
