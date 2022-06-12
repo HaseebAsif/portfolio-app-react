12646c0155793c749c192594e0d4b67cd80dd896
Haseeb Asif <36411814+HaseebAsif@users.noreply.github.com>
2022-06-12 12:45:15 +0500

    React Router Dom Impletemented

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
-import React, { useState, useCallback } from "react";
-var funccount = new Set();
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import About from "./componenets/About";
+import Home from "./componenets/Home";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const [number, setNumber] = useState(0);
-
-  const incrementCounter = useCallback(() => {
-    setCount(count + 1);
-  }, [count]);
-  const decrementCounter = useCallback(() => {
-    setCount(count - 1);
-  }, [count]);
-  const incrementNumber = useCallback(() => {
-    setNumber(number + 1);
-  }, [number]);
-
-  funccount.add(incrementCounter);
-  funccount.add(decrementCounter);
-  funccount.add(incrementNumber);
-  alert(funccount.size);
-
   return (
-    <div>
-      Count: {count}
-      <button onClick={incrementCounter}>Increase counter</button>
-      <button onClick={decrementCounter}>Decrease Counter</button>
-      <button onClick={incrementNumber}>increase number</button>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Routes>
   );
 };
 
